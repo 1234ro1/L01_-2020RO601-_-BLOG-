c5b1826ef28601ed6727c773f151165cf54c057e
omar8 <omar8@LAPTOP-2BEM2ODS>
2023-03-04 10:00:28 -0600

    Agregar archivos de proyecto.

---
+++ laboratorio1/Models/blog.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using System.Collections.Generic;
+
+namespace laboratorio1.Models
+{
+    public class blog : DbContext
+    {
+        public blog(DbContextOptions<blog> options) : base(options)
+        {
+        }
+            
+        public DbSet<blogss>blogss { get; set; }
+    }
+}
---
+++ laboratorio1/Models/blogss.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace laboratorio1.Models
+{
+    public class blogss
+    {
+        public int roles { get; set; }
+        public int usuarios { get; set; }
+        public int publicaciones { get; set; }
+        public int comentarios { get; set;}
+        public int calificaciones { get; set; }
+    }
+}
